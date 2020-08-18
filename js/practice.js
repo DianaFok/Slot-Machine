75113f32c81d8afbaa3fc8570570e17197135884
Diana Fok <diana.y.fok@gmail.com>
2020-08-18 13:07:09 -0400

    first commit

--- js/practice.js
+++
@@ -1,6 +0,0 @@
-const bank = document.getElementById("bank");
-const boxLeft = document.getElementsById("boxLeft");
-const boxRight = document.getElementById("boxRight");
-const boxMid = document.getElementById("boxMid");
-const betDropDown = document.getElementById("bet");
-const playButton = docoument.getElementById("playButton")
